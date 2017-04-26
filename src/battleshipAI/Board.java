7373fdadfd1065d6de777246d4b5e48e533f0b39
Justin Kuang <zk65@cornell.edu>
2017-04-26 18:25:06 -0400

    methods for setting ships

--- src/battleshipAI/Board.java
+++ src/battleshipAI/Board.java
@@ -7,12 +7,21 @@ public class Board {
 	 */
 
 	// size of the board is 16*16
+	
+	//2d array is int[rows][columns]
 	private static int[][] values;
 	private int[][] carrierCoords;
 	private int[][] battleshipCoords;
-	private int[][] cruiserCoords;
-	private int[][] submarineCoords;
 	private int[][] destroyerCoords;
+	private int[][] submarineCoords;
+	private int[][] gunboatCoords;
+	
+	//booleans true if ship is still alive
+	private boolean carrier = true;
+	private boolean battleship = true;
+	private boolean cruiser = true;
+	private boolean submarine = true;	
+	private boolean gunboat = true;
 
 	public Board() {
 		values = new int[16][16];
@@ -23,9 +32,9 @@ public class Board {
 		}
 		carrierCoords = new int[5][2];
 		battleshipCoords = new int[4][2];
-		cruiserCoords = new int[5][2];
-		submarineCoords = new int[5][2];
-		destroyerCoords = new int[5][2];
+		destroyerCoords = new int[3][2];
+		submarineCoords = new int[3][2];
+		gunboatCoords = new int[2][2];
 	}
 
 	public void printBoard() {
@@ -37,9 +46,34 @@ public class Board {
 			}
 		}
 	}
+	//creates sample board for testing
+	private void setSampleBoard(){
+		setPieceVert(carrierCoords,5,1,2);
+		setPieceHoriz(battleshipCoords,12,9,12);
+		
+	}
+	//precondition: startRow > endRow 
+	private void setPieceVert(int[][] piece, int startRow, int endRow, int col){
+		for(int i = 0; i<piece.length; i++){
+			piece[i][0]=startRow-i;
+			piece[i][1]=col;
+			values[startRow-i][col]=1;
+		}
+		
+	}
+	//precondition: startCol > endCol 
+		private void setPieceHoriz(int[][] piece, int startCol, int endCol, int row){
+			for(int i = 0; i<piece.length; i++){
+				piece[i][0]=row;
+				piece[i][1]=startCol-i;
+				values[row][startCol-i]=1;
+			}
+			
+		}
 
 	public static void main(String[] args) {
-		Board board = new Board(10);
+		Board board = new Board();
+		board.setSampleBoard();
 		board.printBoard();
 
 	}
