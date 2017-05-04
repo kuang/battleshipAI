9e81a6c495f7556839b3a74cac2c051546651f7b
Justin Kuang <zk65@cornell.edu>
2017-05-04 13:37:10 -0400

    2d matrix function

--- js/AI.js
+++ js/AI.js
@@ -1,3 +1,46 @@
-var shipGrid = [][];
-var hits = [][];
-var probs = [][];
+function matrix(rows, cols, defaultValue) {
+
+    var arr = [];
+
+    // Creates all lines:
+    for (var i = 0; i < rows; i++) {
+
+        // Creates an empty line
+        arr.push([]);
+
+        // Adds cols to the empty line:
+        arr[i].push(new Array(cols));
+
+        for (var j = 0; j < cols; j++) {
+            // Initializes:
+            arr[i][j] = defaultValue;
+        }
+    }
+
+    return arr;
+}
+
+var shipGrid = matrix(16, 16, 0);
+// var hits = createArray(16, 16);
+// var probs = createArray(16, 16);
+
+var carrier = true;
+var battleship = true;
+var destroyer = true;
+var submarine = true;
+var gunboat = true;
+
+
+//board is a 2d array
+function printBoard(board) {
+    for (var i = 0; i < board.length; i++) {
+        console.log("");
+        console.log("");
+        for (var j = 0; j < board[i].length; j++) {
+            console.log(board[i][j] + "   ")
+        }
+    }
+}
+
+
+printBoard(shipGrid);
