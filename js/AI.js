11b2f16eff951888709462dc05ef6aae3648e634
Justin Kuang <zk65@cornell.edu>
2017-05-08 21:13:39 -0400

    setpiece functions

--- js/AI.js
+++ js/AI.js
@@ -20,15 +20,15 @@ function matrix(rows, cols, defaultValue) {
     return arr;
 }
 
-var shipGrid = matrix(16, 16, 0);
+var shipGrid = matrix(16, 16, "");
 var probs = matrix(16, 16, 0);
 var hits = matrix(16, 16, 0);
 
-var carrier = true;
-var battleship = true;
-var destroyer = true;
-var submarine = true;
-var gunboat = true;
+var carrier = true; //C
+var battleship = true; //B
+var destroyer = true; //D
+var submarine = true; //S
+var gunboat = true; //G
 
 
 //board is a 2d array
@@ -44,6 +44,26 @@ function printBoard(board) {
     }
     console.log(output);
 }
+// creates sample board for testing
+function setSampleBoard() {
+    setPieceVert("C", 5, 1, 2);
+    // setPieceHoriz(battleshipCoords, 12, 9, 12);
 
+}
 
+// precondition: startRow > endRow
+function setPieceVert(letter, startRow, endRow, col) {
+    for (int i = 0; i < startRow - endRow; i++) {
+        values[startRow - i][col] = letter;
+    }
+
+}
+
+// precondition: startCol > endCol
+function setPieceVert(letter, startCol, endCol, row) {
+    for (int i = 0; i < startCol - endCol; i++) {
+        values[row][startCol - i] = letter;
+    }
+
+}
 printBoard(shipGrid);
